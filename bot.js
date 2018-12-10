248911b82a6534e77583aef836299d1377640401
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-10 17:07:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,8 +13,6 @@ client.on("guildMemberAdd", m => {
    
     function datediff(first, second) {
         return Math.round((second-first)/(1000*60*60*24));
-       message.channel.send('!ban <@' + message.author.id + '> 3m')
-       message.delete() 
 };
  
  
