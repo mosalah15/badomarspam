c3e269dbe4687ed39351db1ce029dfa0ef32b03c
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-10 16:13:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
-
-
-client.on("ready", () => {
-let channel =     client.channels.get("513776332614664217")
-setInterval(function() {
-channel.send(`spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam `);
-}, 25)
-})
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
  
  
 client.login(process.env.BOT_TOKEN);
