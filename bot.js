78bee3820f579bd632e104e1938334090f533d0c
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-10 17:55:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
-const moment = require('moment');
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const moment = require('moment');
 client.on ("guildMemberAdd", m => {
     if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
         m.ban();
