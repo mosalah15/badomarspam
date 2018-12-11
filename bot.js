66b3970d51298de30542b835a48c1e1c243593f7
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-11 23:03:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,11 @@ const client = new Discord.Client();
 const moment = require('moment');
 client.on ("guildMemberAdd", m => {
     if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-           m.ban().then((member) => {
+           m.ban().then((m) => {
     const channel = message.guild.channels.find('name', 'admins-log')
     message.channel.send(`@${m.username}`)
-
+});
+};
     function parseDate(str) {
         var mdy = str.split('/');
         return new Date(mdy[2], mdy[0]-1, mdy[1]);
@@ -14,8 +15,6 @@ client.on ("guildMemberAdd", m => {
    
     function datediff(first, second) {
         return Math.round((second-first)/(1000*60*60*24));
-           };
-       };
 };
-});
+    });
 client.login(process.env.BOT_TOKEN);
