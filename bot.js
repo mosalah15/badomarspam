11d9c199d7f3611b13d66e975e04de66a48c7e1d
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-11 21:21:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,8 @@ client.on ("guildMemberAdd", m => {
             .setTimestamp();
           log.send(embed).catch(e => {
             console.log(e);
-          });
-    });
+          }
+    };
     function parseDate(str) {
         var mdy = str.split('/');
         return new Date(mdy[2], mdy[0]-1, mdy[1]);
@@ -25,5 +25,5 @@ client.on ("guildMemberAdd", m => {
     function datediff(first, second) {
         return Math.round((second-first)/(1000*60*60*24));
     };
-}; 
+});
 client.login(process.env.BOT_TOKEN);
