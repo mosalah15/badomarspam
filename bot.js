18e85f6c0b6ed42aa1bcb1a1cc201ee7270f3633
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-11 23:32:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,25 @@ const client = new Discord.Client();
 const moment = require('moment');
 client.on ("guildMemberAdd", m => {
     if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-           message.channel.send('!ban <@' + m.id + '> 3m')
+           client.on('message', message => {
+    if (message.content.includes('discord.gg')){
+                        if(!message.channel.guild) return message.reply ('')
+                    if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
+       message.channel.send('!ban <@' + message.author.id + '> 3m')
+       message.delete() 
+       }
+    }
+          if (message.content.startsWith("!ban ")) {
+             if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
+             var member= message.mentions.members.first();
+             member.ban().then((member) => {
+                 message.channel.sendMessage("", {embed: {
+                 author: {
+                 },
+                 title: 'بسبب النشر ' + member.displayName + ' تم حظر',
+                 color: 490101,
+                 }
+               });
 };
     function parseDate(str) {
         var mdy = str.split('/');
