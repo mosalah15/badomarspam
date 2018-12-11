c6c3ca1204361371c13322a560f4a59d918f102e
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-11 22:59:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,5 +15,7 @@ client.on ("guildMemberAdd", m => {
     function datediff(first, second) {
         return Math.round((second-first)/(1000*60*60*24));
     };
+};
+};
 });
 client.login(process.env.BOT_TOKEN);
