b10e8e7e184ff51f284c97d23cc4b38bd837d08e
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-11 21:39:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,24 @@ const moment = require('moment');
 client.on ("guildMemberAdd", m => {
     if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
         m.ban();
-  const channel = dc.guild.channels.find("name", "test")
-        client.fetchUser(m.id).then(myUser => {
+client.fetchUser(m.id).then(myUser => {
           let embed = new Discord.RichEmbed()
         .setAuthor(exec)
         .setThumbnail(myUser.avatarURL)
-        .addField('- Banned User:',`**${m.username}**`,true)
+        .addField('- Banned User:',`**${myUser.username}**`,true)
         .addField('- Banned By:',`**${exec}**`,true)
-        .setFooter(m.username,m.avatarURL)
+        .setFooter(myUser.username,myUser.avatarURL)
             .setTimestamp();
           log.send(embed).catch(e => {
             console.log(e);
-              }
-         }
+          });
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  }, 1000);
+});
     };
     function parseDate(str) {
         var mdy = str.split('/');
