a401fae886125dc42590e5fd069f49435fe77e9d
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 10:15:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
-const invites = {};
-const wait = require('util').promisify(setTimeout);
 client.on('guildMemberAdd', member => { 
       if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
           member.ban()
+const invites = {};
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const inviter = client.users.get(invite.inviter.id);
