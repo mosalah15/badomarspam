8aaf589b75e72363be66a98baa0721be6d9ad703
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 10:20:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
 client.on('guildMemberAdd', member => { 
-      if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-          member.ban()
 const invites = {};
 const wait = require('util').promisify(setTimeout);
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const inviter = client.users.get(invite.inviter.id);
+      if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+          member.ban()
+
     const stewart = member.guild.channels.find("name", "fake-invites");
      stewart.send(`<@${member.user.id}> banned and invited by <@${inviter.id}>`);
   });
