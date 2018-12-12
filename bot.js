59cc6578084499ee247fe369248ddb501339a212
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 10:20:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ const wait = require('util').promisify(setTimeout);
 
     const stewart = member.guild.channels.find("name", "fake-invites");
      stewart.send(`<@${member.user.id}> banned and invited by <@${inviter.id}>`);
-  });
-};
+  };
+});
     function parseDate(str) {
         var mdy = str.split('/');
         return new Date(mdy[2], mdy[0]-1, mdy[1]);
