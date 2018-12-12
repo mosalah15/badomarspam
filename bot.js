7fe652a81774797870bb1ea0573740989609600b
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 10:07:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,15 @@ const client = new Discord.Client();
 const moment = require('moment');
 const invites = {};
 const wait = require('util').promisify(setTimeout);
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+client.on('guildMemberAdd', member => { 
+      if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+          member.ban()
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-    if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
-          member.ban()
-        const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+    const stewart = member.guild.channels.find("name", "fake-invites");
+     stewart.send(`<@${member.user.id}> banned and invited by <@${inviter.id}>`);
   };
 });
     function parseDate(str) {
