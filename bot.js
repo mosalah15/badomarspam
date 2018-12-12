7172206e0bc02445748eca44fceb1bd895fd9179
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 11:55:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const invites = {};
 client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
