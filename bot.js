e6dcd0ac1ed155ee866926be909c2fd5eaeb3617
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 11:06:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ const invites = {};
 client.on('guildMemberAdd', member => { 
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(member.inviter.id);
+    const inviter = client.users.get(invites.inviter.id);
       if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
           member.ban()
 
