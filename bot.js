aa6f940b195ff372a19167be9464ff621cc78030
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 09:31:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,13 +5,16 @@ client.on ("guildMemberAdd", member => {
     if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
           member.ban()
 
-   member.guild.fetchInvites().then(guildInvites => {
+client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+  member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(inviter.id);
+    const inviter = client.users.get(invite.inviter.id);
     const stewart = member.guild.channels.find("name", "fake-invites");
-     stewart.send(`<@${member.user.id}> has been banned as a fake invite (invited By  <@${inviter.id}>) `);
-});
+     stewart.send(`<@${member.user.id}> banned as fake invite <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  });
+})
 };
     function parseDate(str) {
         var mdy = str.split('/');
