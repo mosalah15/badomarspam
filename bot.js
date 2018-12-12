096a8191af421ca3dc564e9e8435654a27b17330
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 11:59:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const moment = require('moment');
 const invites = {};
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+client.on('guildMemberAdd', m => { 
   member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
+    const ei = invites[m.guild.id];
+    invites[m.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  });
-})
-client.login(process.env.BOT_TOKEN);
+      if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+          m.ban()
+const logChannel = m.guild.channels.find(channel => channel.name === "fake-invites");
+    logChannel.send(`${m} has been banned Invited by: <@${inviter.id}>`)
+  };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+};
+    });
