fe970e9561af7f6176ab38b206ee3be3b8e74489
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 09:02:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,11 @@ const moment = require('moment');
 client.on ("guildMemberAdd", m => {
     if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
           m.ban()
+
+  m.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[m.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
             const stewart = m.guild.channels.find("name", "admins-log");
      stewart.send(`<@${m.user.id}> has been banned as a fake invite (invited By  <@${inviter.id}>) `);
 };
