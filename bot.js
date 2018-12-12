f98b43d0ec042a3aef5e73d591449d5ab38f7d23
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-12 11:50:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const moment = require('moment');
-const invites = {};
-const wait = require('util').promisify(setTimeout);
-const Util = require('discord.js');
-const get = require('discord.js');
+
 client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
