dfbb08fb098c6705433e2193633295a3bdd915c1
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-15 22:41:29 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
-const Discord = require('discord.js');
+﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
 const invites = {};
-const db = require('quick.db');
-const env = require('../env');
+
 const wait = require('util').promisify(setTimeout);
 
 client.on('ready', () => {
-  wait(100000);
+  wait(1000);
 
   client.guilds.forEach(g => {
     g.fetchInvites().then(guildInvites => {
@@ -15,24 +14,18 @@ client.on('ready', () => {
     });
   });
 });
-client.on('message', async (message) => {
-    var prefix = '1';
-    var fetchedPrefix = await db.fetch(`serverPrefix_${message.guild.id}`);
-    if (fetchedPrefix === null || typeof fetchedPrefix === 'undefined') fetchedPrefix = prefix;
-    else prefix = fetchedPrefix;
-
-         
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
+client.on('guildMemberAdd', m => { 
+  m.guild.fetchInvites().then(guildInvites => {
+const ei = invites[m.guild.id];
+    invites[m.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-      if (datediff(parseDate(moment(member.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < fetchedPrefix  ) {
-          member.ban()
-const logChannel = member.guild.channels.find(channel => channel.name === "fake-invites");
-    logChannel.send(`${member} has been banned as a fake account ***Invited by: <@${inviter.id}>***`)
-    }
-})
+      if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+          m.ban()
+const logChannel = m.guild.channels.find(channel => channel.name === "fake-invites");
+    logChannel.send(`${m} has been banned as a fake account ***Invited by: <@${inviter.id}>***`)
+  };
+  });  
     function parseDate(str) {
         var mdy = str.split('/');
         return new Date(mdy[2], mdy[0]-1, mdy[1]);
@@ -41,6 +34,5 @@ const logChannel = member.guild.channels.find(channel => channel.name === "fake-
     function datediff(first, second) {
         return Math.round((second-first)/(1000*60*60*24));
 };
-})
     });
 client.login(process.env.BOT_TOKEN); 
