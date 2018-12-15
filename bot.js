b1e0e5587a5b62229f04e8e37e294cb1de53934a
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-15 16:43:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ const ei = invites[m.guild.id];
     invites[m.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-      if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 1) {
+      if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
           m.ban()
 const logChannel = m.guild.channels.find(channel => channel.name === "fake-invites");
     logChannel.send(`${m} has been banned as a fake account ***Invited by: <@${inviter.id}>***`)
@@ -31,7 +31,7 @@ const logChannel = m.guild.channels.find(channel => channel.name === "fake-invit
     };
    
     function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24*15));
+        return Math.round((second-first)/(1000*60*60*24));
 };
     });
 client.login(process.env.BOT_TOKEN); 
