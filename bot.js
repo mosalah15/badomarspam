9076b243b640060bf8fc91d6512492b6b3849a93
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-16 14:01:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,13 @@
 const client = new Discord.Client();
 const moment = require('moment');
 const invites = {};
-const db = require('quick.db');
 const wait = require('util').promisify(setTimeout);
 
 client.on = (client, message, args) => {
   if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You don\'t have permission to set server prefix.');
   if (!args.join(' ')) return message.channel.send('Please provide a prefix to set server prefix.');
   
-  db.set(`serverPrefix_${message.guild.id}`, args.join(' ')).then((serverPrefix) => {
+  lesstime.set(`serverPrefix_${message.guild.id}`, args.join(' ')).then((serverPrefix) => {
     message.channel.send(`Server Prefix has been set to ${serverPrefix}`);
   });
 };
@@ -24,7 +23,7 @@ client.on('ready', () => {
 });
 client.on('message', async (message) => {
     var prefix = '!';
-    var fetchedPrefix = await db.fetch(`serverPrefix_${message.guild.id}`);
+    var fetchedPrefix = await lesstime.fetch(`serverPrefix_${message.guild.id}`);
     if (fetchedPrefix === null || typeof fetchedPrefix === 'undefined') fetchedPrefix = prefix;
     else prefix = fetchedPrefix;
   });
@@ -34,7 +33,7 @@ const ei = invites[m.guild.id];
     invites[m.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-      if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 8) {
+      if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < lesstime) {
           m.ban()
 const logChannel = m.guild.channels.find(channel => channel.name === "fake-invites");
     logChannel.send(`${m} has been banned as a fake account ***Invited by: <@${inviter.id}>***`)
