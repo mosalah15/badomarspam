943328c4345eb80551d1c6725277828bcd6c78f8
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-16 13:49:58 +0200

    Delete purge.js

--- commands/purge.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message, args) => {
-  if(isNaN(args[0])) return message.channel.send('Please provide a valid amount to purge or delete messages!');
-  if (args[0] > 100) return message.channel.send('Supply a amount less than 100!');
-  message.channel.bulkDelete(args[0]);
-};
