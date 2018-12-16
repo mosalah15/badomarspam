4ed75c2b7ab9fb996f1046cff448a1d4036d140c
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-16 13:54:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,14 @@ const invites = {};
 const db = require('quick.db');
 const wait = require('util').promisify(setTimeout);
 
+exports.run = (client, message, args) => {
+  if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You don\'t have permission to set server prefix.');
+  if (!args.join(' ')) return message.channel.send('Please provide a prefix to set server prefix.');
+  
+  db.set(`serverPrefix_${message.guild.id}`, args.join(' ')).then((serverPrefix) => {
+    message.channel.send(`Server Prefix has been set to ${serverPrefix}`);
+  });
+};
 client.on('ready', () => {
   wait(1000);
 
