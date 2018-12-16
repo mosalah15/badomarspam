b720e647155e877946430b300fab369334112559
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-16 21:26:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('ready', () => {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
   if (!args.join(' ')) return message.channel.send('Please provide a prefix to set server prefix.');
   var lesstime = (args.slice(1, args.length).join(" "))
-client.on ("guildRolesAdd", (member, guild)=> {
+client.on ("guildRoleUpdate", (member, guild)=> {
   client.setTimeout(() => {
     guild.fetchAuditLogs({
         limit: 1,
