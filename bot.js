f9d1fbb50ee32f842404b60032fc54f8f022e0f5
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-16 14:02:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const invites = {};
 const wait = require('util').promisify(setTimeout);
 
 client.on = (client, message, args) => {
-  if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You don\'t have permission to set server prefix.');
+  if (!message.member.hasPermission('Administrator')) return message.channel.send('You don\'t have permission to set server prefix.');
   if (!args.join(' ')) return message.channel.send('Please provide a prefix to set server prefix.');
   
   lesstime.set(`serverPrefix_${message.guild.id}`, args.join(' ')).then((serverPrefix) => {
