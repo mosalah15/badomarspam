4967a5e319bf5ef0e12188839035639071c6eeff
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2018-12-18 20:11:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('ready', () => {
   client.on('guildMemberUpdate', (oldMember, newMember) => {
     oldMember.guild.fetchAuditLogs().then(LastSeen => {
                // null
-      if (datediff(parseDate(moment(newMember.user.lastMessage)).format('l')), parseDate(moment().format('l'))) > 1) {
+      if (datediff(parseDate(moment(newMember.user.lastMessage)).format('l')), parseDate(moment().format('l'))) < 1) {
 		newMember.ban()
 	      };
 });
